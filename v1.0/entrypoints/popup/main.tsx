f0e30f2e72518a95fee163a9f0876b106e0fb7ea
Tanmay Jain <tjstar09@gmail.com>
2026-07-17 23:36:58 +0100

    refactor(css): split monolithic styles.css into modular architecture (Phase 3)

--- v1.0/entrypoints/popup/main.tsx
+++ v1.0/entrypoints/popup/main.tsx
@@ -19,7 +19,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from '../sidepanel/src/App';
-import '../sidepanel/src/styles.css';
+import '../sidepanel/src/styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- v1.0/entrypoints/sidepanel/main.tsx
+++ v1.0/entrypoints/sidepanel/main.tsx
@@ -10,7 +10,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './src/App';
-import './src/styles.css';
+import './src/styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
